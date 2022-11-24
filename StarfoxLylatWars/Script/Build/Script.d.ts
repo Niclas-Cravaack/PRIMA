94d3f258363ee0e7350cd706cd17a50bcf4f1155
Niclas-Cravaack <60705949+Niclas-Cravaack@users.noreply.github.com>
2022-11-24 15:05:59 +0100

    dasdasdaw

--- StarfoxLylatWars/Script/Build/Script.d.ts
+++ StarfoxLylatWars/Script/Build/Script.d.ts
@@ -8,4 +8,33 @@ declare namespace Script {
     }
 }
 declare namespace Script {
+    import ƒ = FudgeCore;
+    class SpaceShipMovement extends ƒ.ComponentScript {
+        static readonly iSubclass: number;
+        message: string;
+        private rgdBodySpaceship;
+        private pitchF;
+        private rollF;
+        private yawf;
+        strafeThrust: number;
+        forwardthrust: number;
+        private relativeX;
+        private relativeY;
+        private relativeZ;
+        constructor();
+        hndEvent: (_event: Event) => void;
+        update: () => void;
+        private width;
+        private height;
+        private xAxis;
+        private yAxis;
+        handleMouse: (e: MouseEvent) => void;
+        setRelativeAxes(): void;
+        backwards(): void;
+        thrust(): void;
+        rollLeft(): void;
+        rollRight(): void;
+    }
+}
+declare namespace Script {
 }
--- StarfoxLylatWars/Script/Source/Main.ts
+++ StarfoxLylatWars/Script/Source/Main.ts
@@ -3,18 +3,29 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  
+  
+  
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    let graph = viewport.getBranch();
+    let shipPos = graph.getChildrenByName("InterceptorPos")[0];
+    let ship = shipPos.getChild(0);
+    let shipRigidBody = ship.getComponent(ƒ.ComponentRigidbody);
+    shipRigidBody.applyForce(ƒ.Vector3.Z(1000));
 
+    
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+   ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.simulate();  // if physics is included and used
+     ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+
+  
 }
